26f8703633b9b1e5f91075f02c6a4ec0aae62b74
Pavel Petrov <pavel.77.petrov.7@abv.bg>
2021-02-11 21:58:38 +0200

    WIMRepositoryImpl_Test almost finished

--- oop-project/Skeleton/Tests/core/WIMRepositoryImpl_Test.java
+++ oop-project/Skeleton/Tests/core/WIMRepositoryImpl_Test.java
@@ -1,23 +1,57 @@
 package core;
 
 import com.telerikacademy.oop.wim.core.WIMRepositoryImpl;
-import com.telerikacademy.oop.wim.models.TeamImpl;
+import com.telerikacademy.oop.wim.models.*;
+import models.BoardImpl_Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WIMRepositoryImpl_Test {
 
     public WIMRepositoryImpl wimRepository;
+    public TeamImpl teamTest;
+    public MemberImpl memberTest;
+    public BoardImpl boardTest;
+
+    public BugImpl bug;
+    public StoryImpl story;
+    public FeedbackImpl feedback;
 
     @BeforeEach
     public void beforeEach(){
         wimRepository = new WIMRepositoryImpl();
+        teamTest = new TeamImpl("TestTeam");
+        memberTest = new MemberImpl("Monika");
+        boardTest = new BoardImpl("TestBoard");
+
+        bug = BoardImpl_Test.bugTest;
+        story = BoardImpl_Test.storyTest;
+        feedback = BoardImpl_Test.feedbackTest;
     }
 
     @Test
-    public void checkIf_WIMRepository_adds_TeamImpl_WithValidArguments(){
-        TeamImpl teamTest = new TeamImpl("TestTeam");
-
+    public void checkIf_WIMRepository_adds_TeamImpl_withValidArguments(){
         wimRepository.addTeam("TestTeam", teamTest);
+
+        Assertions.assertEquals(wimRepository.getTeams().get("TestTeam"), teamTest);
     }
+
+    @Test
+    public void checkIf_WIMRepository_adds_MemberImpl_withValidArguments(){
+        wimRepository.addMember("Monika", memberTest);
+
+        Assertions.assertEquals(wimRepository.getMembers().get("Monika"), memberTest);
+    }
+
+    @Test
+    public void checkIf_WIMRepository_adds_TeamImpl_withInvalidArguments(){
+        wimRepository.addTeam("TestTeam", teamTest);
+
+        //wimRepository should add teams with unique name
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> wimRepository.addTeam("TestTeam", teamTest));
+    }
+
+
 }
